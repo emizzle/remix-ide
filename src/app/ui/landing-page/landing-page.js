da063819e085ab17169c788f84f2513ccae26cf4
LianaHus <liana@ethdev.com>
2019-03-15 22:17:54 +0100

    fixed context menu style

--- src/app/ui/landing-page/landing-page.js
+++ src/app/ui/landing-page/landing-page.js
@@ -92,7 +92,7 @@ export class LandingPage extends ApiFactory {
 
   get profile () {
     return {
-      displayName: 'home',
+      displayName: 'Home',
       name: 'home',
       methods: [],
       events: [],
